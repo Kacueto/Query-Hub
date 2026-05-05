cf70637bd46197949066b025b6e00ddd0bf732f2
Darlen Arias <darlena@uninorte.edu.co>
2026-05-04 20:37:53 -0500

    feat(infrastructure): define UsersModule and dependency injections

---
+++ apps/api/src/presentation/modules/users/users.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserTypeormEntity } from "../../../infrastructure/persistence/entities/user.typeorm-entity";
+import { USER_REPOSITORY } from "../../../domain/repositories/user.repository";
+import { PostgresUserRepository } from "../../../infrastructure/persistence/repositories/postgres-user.repository";
+import { CreateUserUseCase } from "../../../application/use-cases/users/create-user.use-case";
+import { GetAllUsersUseCase } from "../../../application/use-cases/users/get-all-users.use-case";
+import { GetUserByIdUseCase } from "../../../application/use-cases/users/get-user-by-id.use-case";
+import { DeleteUserUseCase } from "../../../application/use-cases/users/delete-user.use-case";
+import { UsersController } from "../../http/controllers/users.controller";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserTypeormEntity])],
+  providers: [
+    { provide: USER_REPOSITORY, useClass: PostgresUserRepository },
+    CreateUserUseCase,
+    GetAllUsersUseCase,
+    GetUserByIdUseCase,
+    DeleteUserUseCase,
+  ],
+  controllers: [UsersController],
+  exports: [USER_REPOSITORY], // lo exporta para que AuthModule pueda usarlo
+})
+export class UsersModule {}
